4bc0506574e90a1986fb1a8856821545702c3c91
antsaaims <antsa@aims.ac.za>
2020-07-14 18:54:25 +0200

    after defining building

--- app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
+++ app/src/main/java/mooc/vandy/java4android/buildings/logic/Building.java
@@ -6,5 +6,60 @@ package mooc.vandy.java4android.buildings.logic;
 public class Building {
     
     // TODO - Put your code here.
-    
+private int mLength;
+private int mWidth;
+private int mLotLength;
+private int mLotWidth;
+
+public Building(int length,int width,int lotLength,int lotWidth)//The constructor
+         {
+       this.mLength=length;
+       this.mWidth=width;
+       this.mLotLength=lotLength;
+       this.mLotWidth=lotWidth;
+
+         }
+public int  getLength(){
+      return this.mLength;
+     }
+
+public int getWidth(){
+    return this.mWidth;
+    }
+public int  getLotLength(){
+    return this.mLotLength;
+    }
+public int intgetLotWidth(){
+
+    return this.mLotWidth;
+    }
+public void setLength(int length){
+    this.mLength=length;
+
+    }
+public void setWidth(int width){
+    this.mWidth=width;
+
+    }
+public void setLotLength(int lotLength){
+    this.mLotLength=lotLength;
+
+    }
+public void setLotWidth(int lotWidth){
+
+    this.mLotWidth=lotWidth;
+    }
+public int calcBuildingArea(){
+    return this.mWidth*this.mLength;
+
+    }
+public int calcLotArea(){
+    return this.mLotWidth*this.mLotLength;
+
+
+   }
+public String toString(){
+   return "This is a Building object";
+   }
+
 }
--- app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
+++ app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
@@ -7,5 +7,30 @@ public class House
        extends Building {
        
     // TODO - Put your code here.
-    
+private String mOwner;
+private Boolean mPool;
+
+public House(int length,int width,int lotLength,int lotWidth)//constructor
+        {
+            super(length, width, lotLength, lotWidth);
+            this.mOwner="n/a";
+            this.mPool=false;
+
+        }
+public House(int length,int width,int lotLength,int lotWidth,String owner)//constructor
+        {
+            this(length, width, lotLength, lotWidth);
+            this.mOwner=owner;
+
+        }
+
+public House(intlength,intwidth,intlotLength,intlotWidth,Stringowner,boolean
+            pool)//constructor
++getOwner():String
++hasPool():boolean
++setOwner(String):void
++setPool(boolean):void
+
+
+
 }
